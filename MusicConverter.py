74d752971aa36355a22867f750731e90e351eda9
Bryan <161897645+Keepas3@users.noreply.github.com>
2024-10-29 10:56:25 -0400

    Update MusicConverter.py
    Paths contains less sensitive info

--- MusicConverter.py
+++ MusicConverter.py
@@ -5,7 +5,7 @@ def convert_mp4_to_flac(mp4_path, flac_path):
     audio = video.audio
     audio.write_audiofile(flac_path, codec='flac')
 
-mp4_path = "C:\\Users\\fungb\\Downloads\\Chain Gang of 1974 -  Sleepwalking [OFFICIAL HQ STREAM]\Chain Gang of 1974 -  Sleepwalking [OFFICIAL HQ STREAM] (1080p_24fps_H264-128kbit_AAC).mp4" # copy as path the mp4 file
-flac_path = "C:\\Users\\fungb\OneDrive\\Desktop\\Converted Songs\\Sleepwalking.flac"  # Specify where you want the file to go and file name
+mp4_path = "C:\\Users\\Username\\Downloads\\Chain Gang of 1974 -  Sleepwalking [OFFICIAL HQ STREAM]\Chain Gang of 1974 -  Sleepwalking [OFFICIAL HQ STREAM] (1080p_24fps_H264-128kbit_AAC).mp4" # copy as path the mp4 file
+flac_path = "C:\\Users\\username\OneDrive\\Desktop\\Folder to addSongs\\Sleepwalking.flac"  # Specify where you want the file to go and file name
 
 convert_mp4_to_flac(mp4_path, flac_path)
