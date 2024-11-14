ee63f99c7c7be258711d7e7228f31be55d5460ff
Keepas3 <bfun679@gmail.com>
2024-11-14 14:29:38 -0500

    Result webpage works correctly

--- Server.py
+++ Server.py
@@ -13,17 +13,17 @@ def convert_video_to_flac(video_path, flac_path):
     audio.write_audiofile(flac_path, codec='flac')
 
 @Server.route('/')
-def home():
+def home(): # Tells the server to render the home page or the Converter.html when they type in the URL
     return render_template('Converter.html')
 
-@Server.route('/convert', methods=['POST'])
+@Server.route('/convert', methods=['POST']) # maps the /convert URL to convert function
 def convert(): 
-    mp4_path = request.form['mp4_path']
-    mp4_path = mp4_path.replace('"', '').replace("'", "")
+    mp4_path = request.form['mp4_path'] # retrieves the value of the text field typed in by the user
+    mp4_path = mp4_path.replace('"', '').replace("'", "") #replaces any quotes 
     
     title = extract_title_from_path(mp4_path)
-    flac_path = f"C:\\Users\\bryan\\OneDrive\\Desktop\\New folder (2)\\{title}.flac"
-  #  flac_path = f"C:\\Users\\fungb\\OneDrive\\Desktop\\Converted Songs\\{title}.flac"
+  #  flac_path = f"C:\\Users\\bryan\\OneDrive\\Desktop\\New folder (2)\\{title}.flac"
+    flac_path = f"C:\\Users\\fungb\\OneDrive\\Desktop\\Converted Songs\\{title}.flac"
     
     try:
         convert_video_to_flac(mp4_path, flac_path)
@@ -31,7 +31,8 @@ def convert():
     except Exception as e:
         message = f"Conversion failed: {str(e)}"
 
-    return render_template('Conversion_Successful_page.html', flac_path = flac_path ,message = message)
+    print("Rendering template with message:", message)
+    return render_template('Result.html')
     #return f"Conversion complete! FLAC file saved at {flac_path}"
 
 def extract_title_from_path(path):
